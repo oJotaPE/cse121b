60628f119a233058cbba1725ae634e6f420bad5d
João P. <65201780+oJotaPE@users.noreply.github.com>
2024-03-22 10:09:39 -0300

    learning activities

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,91 +1,59 @@
-// var y = 8;
-// var x = 10;
-// var stringA = "I like ";
-// var stringB = "french fries."
-// var equal;
-// var less;
-// var comparison;
+async function populate() {
+  const requestURL = 
+    "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+  const request = new Request(requestURL);
+  
+   const response = await fetch(request);
+   const superHeroes = await response.json();
 
-// let sum = x + y;
-// let multiply = x * y;
-// let subtraction = x - y;
-// let concatinate = stringA + stringB;
-// let incremetx = x += 3
-
-// if ( x !== y)
-// {
-//     equal = "different";
-// }
-// if ( y < x)
-// {
-//     less = "y is smaller";
-// }
-// if (y < 10 && y > 0)
-// {
-//     comparison = "y is perfect!"
-// }
-
-
-// const display = `- sum: ${sum} \n- multiply: ${multiply} \n- subtraction: ${subtraction} \n- concatinate: ${concatinate} \n- Increment the value of x: ${incremetx} \n- are they the same number? ${equal} \n- which is smaller: ${less} \n- is y smaller than 10 and greater than 0? ${comparison}`;
-// console.log(display);
-
-
-
-// let output = "Welcome to " + courseSubject + " " + courseNumber + "!Credits: " + courseCredits + "Length: " + courseLength;
-// let display = `Welcome to ${courseSubject} ${courseNumber} 
-// !Creditds: ${courseCredits} 
-// Length: ${courseLength}`;
-
-
-// let temples = ["Rome", "Salt Lake", "Nauvoo"];
-// let listItems = "";
-// for (const temple of temples) {
-//   listItems += `<li>${temple}</li>`;
-// }
-
-// document.querySelector("ul").innerHTML = listItems;
-
-// const link = document.querySelector("a");
-// link.textContent = "Mozilla Developer Network";
-// link.href = "https://developer.mozilla.org"; 
-
-// const sect = document.querySelector("section");
-// const para = document.createElement("p");
-// para.textContent = "Hope you enjoy the ride. "
-// sect.appendChild(para);
-
-// const text = document.createTextNode(
-//   " - the premier source for web development knowledge."
-// );
-// const linkPara = document.querySelector("p");
-// linkPara.appendChild(text);
-
-// sect.appendChild(linkPara);
-
-// // para.style.color = "white";
-// // para.style.backgroundColor = "black";
-// // para.style.padding = "10px";
-// // para.style.width = "250px";
-// // para.style.textAlign = "center";
-// para.setAttribute("class", "highlight");
-
-// let firstName = 'Antonia';
-// let lastName = 'Francesca';
-
-// const fullName = (first, last) => `${first} ${last}`;
-// const paragraph = document.querySelector('p');
-// paragraph.innerHTML = fullName(firstName, lastName);
-
-let names = ['Nancy','Blessing','Jorge','Svetlana','Bob'];
-
-let namesB = names.filter((name) => name.charAt[0] === 'B');
-let namesLength = names.map((name) => name.length);
-const average = (total, number, array) => {
-    total += number;
-    final = total / array.length;
-    return final;
+   populateHeader (superHeroes);
+   populateHeroes (superHeroes);
 }
-let namesC = names.reduce((total, name) => total + name.length, 0)/names.length;
-console.log(namesC);
 
-// let namesC = names.reduce((accumulator, current) => (current.length + accumulator) );
+ function populateHeader(obj) {
+  const header = document.querySelector("header");
+  const myH1 = document.createElement("h1");
+
+  myH1.textContent = obj.squadName;
+  header.appendChild(myH1);
+
+  const myPara = document.createElement("p");
+  myPara.textContent = `HomeTown: ${obj.homeTown} // formed: ${obj.formed}`;
+  header.appendChild(myPara);
+ }
+
+ function populateHeroes(obj) {
+  const section = document.querySelector("section");
+  const heroes = obj.members;
+
+  for (const hero of heroes) {
+    const myArticle = document.createElement("article");
+    const myH2 = document.createElement("h2");
+    const myPara1 = document.createElement("p");
+    const myPara2 = document.createElement("p");
+    const myPara3 = document.createElement("p");
+    const myList = document.createElement("ul");
+
+    myH2.textContent = hero.name;
+    myPara1.textContent = `Secret identity: ${hero.secretIdentity}`;
+    myPara2.textContent = `Age: ${hero.age}`;
+    myPara3.textContent = "Superpowers:";
+
+    const superPowers = hero.powers;
+    for (const power of superPowers) {
+      const listItem = document.createElement("li");
+      listItem.textContent = power;
+      myList.appendChild(listItem);
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
+ }
+
+ populate();
